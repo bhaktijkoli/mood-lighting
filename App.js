a1545cad1298ce196e8949784ee72143548dfde1
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-02-03 03:23:01 +0530

    Added color picker

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { ColorWheel } from 'react-native-color-wheel';
 
 export default class App extends React.Component {
+  handleChange(color) {
+
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+        <ColorWheel
+          initialColor="#ee0000"
+          onColorChange={this.handleChange.bind(this)}
+          style={{width: Dimensions.get('window').width}}
+          thumbStyle={{ height: 30, width: 30, borderRadius: 30}} />
+        </View>
+      );
+    }
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#eee',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
